df597839128a70f67c759d3bff7de31199b27a6a
immanaro2497 <immanuel2497@gmail.com>
2025-07-12 10:21:43 +0530

    Initial Commit

---
+++ TransformService/TransformServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransformServiceApp.swift
+//  TransformService
+//
+//  Created by Immanuel on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TransformServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
